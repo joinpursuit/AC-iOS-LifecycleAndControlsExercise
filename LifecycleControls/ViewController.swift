bcf2dc9b04411f14078a42bb5b9ce932af9e8f85
Tom Seymour <tomseymour44@gmail.com>
2017-11-07 11:56:31 -0500

    empty master

--- LifecycleControls/ViewController.swift
+++ LifecycleControls/ViewController.swift
@@ -8,47 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
-    var songs = Song.loveSongs
+class ViewController: UIViewController {
     
-    @IBOutlet weak var tableView: UITableView!
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        self.tableView.reloadData()
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.delegate = self
-        self.tableView.dataSource = self
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return min(songs.count, Settings.maxNumberOfRows)
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let song = songs[indexPath.row]
-        let cell = self.tableView.dequeueReusableCell(withIdentifier: "Song Cell", for: indexPath)
-        switch Settings.capitalization {
-        case .lower:
-            cell.textLabel?.text = song.name.lowercased()
-            cell.detailTextLabel?.text = song.artist.lowercased()
-        case .upper:
-            cell.textLabel?.text = song.name.uppercased()
-            cell.detailTextLabel?.text = song.artist.uppercased()
-        case .proper:
-            //TO DO
-            cell.textLabel?.text = song.name.capitalized
-            cell.detailTextLabel?.text = song.artist.capitalized
-        }
-        cell.backgroundColor = UIColor(displayP3Red: Settings.cellColor.red, green: Settings.cellColor.green, blue: Settings.cellColor.blue, alpha: 1.0)
-        if Settings.alternatingCells && indexPath.row % 2 == 0 {
-            cell.backgroundColor = UIColor(displayP3Red: 1 - Settings.cellColor.red, green: 1 - Settings.cellColor.green, blue: 1 - Settings.cellColor.blue, alpha: 1.0)
-        }
-        return cell
+        
     }
     
 }
